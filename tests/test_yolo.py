fee966e750fb651227b9d53f4142c025a9b49982
Akash Chakraborty <Akash.Chakraborty@procentrisin.com>
2025-09-16 18:14:22 +0530

    Updated for Update YOLO weights v5

--- tests/test_yolo.py
+++ tests/test_yolo.py
@@ -7,7 +7,9 @@ def test_yolo_inference():
     # Run prediction
     results = model.predict("./data/images/val/image_1235.jpg", imgsz=640)
 
-    # Access detections safely
-    boxes = results[0].boxes
-    assert boxes is not None, "No boxes object returned"
-    assert boxes.data.shape[0] > 0, "YOLO did not detect any objects"
+    # Access the results
+    for result in results:
+        xywhr = result.obb.xywhr  # center-x, center-y, width, height, angle (radians)
+        xyxyxyxy = result.obb.xyxyxyxy  # polygon format with 4-points
+        names = [result.names[cls.item()] for cls in result.obb.cls.int()]  # class name of each box
+        confs = result.obb.conf  # confidence score of each box
